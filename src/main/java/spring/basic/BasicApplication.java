f10e600fa622b946c1994e713d37933e432edd80
kwanghee <rhkd1129@gmail.com>
2023-06-24 14:58:53 +0900

    Member 패키지 및 클래스 생성

--- src/main/java/spring/basic/BasicApplication.java
+++ src/main/java/spring/basic/BasicApplication.java
@@ -9,6 +9,7 @@ public class BasicApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BasicApplication.class, args);
 		//git push 를 위한 변경점 만들기 _ jangsik
+		//member 추가 확인 (kkh)
 	}
 
 }
